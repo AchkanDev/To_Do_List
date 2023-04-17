851a3c2a3ce038e6711e4431bffacd9c16a08e03
Ashkan <ashkan.abavi1@gmail.com>
2023-04-17 19:27:03 +0430

    Add feature edit and delete tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
             onSecondary: Colors.white),
       ),
       debugShowCheckedModeBanner: false,
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -69,7 +70,8 @@ class MyHomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
             Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => EditTaskScreen(tasks: Tasks(),)));
+                MaterialPageRoute(
+                    builder: (context) => EditTaskScreen(tasks: Tasks(),)));
           },
           label: const Text("Add New Task")),
       body: SafeArea(
@@ -119,7 +121,7 @@ class MyHomePage extends StatelessWidget {
               height: 110,
               decoration: const BoxDecoration(
                 gradient:
-                    LinearGradient(colors: [primaryColor, primaryVariantColor]),
+                LinearGradient(colors: [primaryColor, primaryVariantColor]),
               ),
             ),
             Expanded(
@@ -128,7 +130,7 @@ class MyHomePage extends StatelessWidget {
                 builder: (BuildContext context, box, Widget? child) {
                   return ListView.builder(
                     padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
-                    itemCount: box.values.length +1 ,
+                    itemCount: box.values.length + 1,
                     itemBuilder: (BuildContext context, int index) {
                       if (index == 0) {
                         return Row(
@@ -137,10 +139,19 @@ class MyHomePage extends StatelessWidget {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  "Person",
-                                  style: themData.textTheme.headline6!
-                                      .apply(fontSizeFactor: 0.9),
+                                Row(
+                                  mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Person",
+                                      style: themData.textTheme.headline6!
+                                          .apply(fontSizeFactor: 0.9),
+                                    ),
+                                    SizedBox(width: MediaQuery.of(context).size.width/2,),
+                                    ElevatedButton(onPressed: (){
+                                      box.clear();
+                                    }, child: Text("Delete All"))
+                                  ],
                                 ),
                                 Container(
                                   width: 70,
@@ -155,7 +166,7 @@ class MyHomePage extends StatelessWidget {
                           ],
                         );
                       } else {
-                        final Tasks task = box.values.toList()[index-1];
+                        final Tasks task = box.values.toList()[index - 1];
                         return TaskItem(task: task);
                       }
                     },
@@ -183,82 +194,103 @@ class _TaskItemState extends State<TaskItem> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      height: 84,
-      padding: const EdgeInsets.only(left: 16, right: 16),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(19),
-          color: theme.colorScheme.surface,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 20,
-            )
-          ]),
-      child: Row(
-        children: [
-          InkWell(
-              onTap: () {
-                setState(() {
-                  widget.task.isCompleted = !widget.task.isCompleted;
-                });
-              },
-              child: MyCheckBox(widget.task.isCompleted , primaryColor)),
-          const SizedBox(
-            width: 16,
-          ),
-          Text(widget.task.name,
-              style: TextStyle(
-                decoration:
+    Color colorPriority;
+    switch (widget.task.priority) {
+      case Priority.high:
+        colorPriority = Colors.redAccent;
+        break;
+
+      case Priority.medium:
+        colorPriority = Colors.yellowAccent;
+        break;
+
+      case Priority.low:
+        colorPriority = Colors.cyanAccent;
+        break;
+    }
+    return InkWell(
+      onLongPress: (){
+        widget.task.delete();
+      },
+      onTap: (){
+        setState(() {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditTaskScreen(tasks: widget.task)));
+        });
+      },
+      child: Container(
+        height: 84,
+        padding: const EdgeInsets.only(left: 16),
+        margin: EdgeInsets.only(top: 8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: theme.colorScheme.surface,
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 0),
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 20,
+              )
+            ]),
+        child: Row(
+          children: [
+            MyCheckBox(isCompleted: widget.task.isCompleted , onTap: (){
+              setState(() {
+                widget.task.isCompleted = !widget.task.isCompleted;
+              });
+            }),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Text(widget.task.name,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    decoration:
                     widget.task.isCompleted ? TextDecoration.lineThrough : null,
-              )),
-        ],
+                  )),
+            ),
+            Container(
+              height: 84,
+              width: 8,
+              decoration: BoxDecoration(
+                color: colorPriority,
+                borderRadius: BorderRadius.only(topRight: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class MyCheckBox extends StatelessWidget {
-  final Color? color;
   final bool isCompleted;
+  final GestureTapCallback onTap;
 
-  MyCheckBox(this.isCompleted, [this.color]);
+  MyCheckBox({required this.isCompleted,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 24,
-      height: 24,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        border: !isCompleted
-            ? Border.all(color: secondaryTextColor, width: 2)
-            : null,
-        color: isCompleted ? color : null,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 24,
+        height: 24,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: !isCompleted
+              ? Border.all(color: secondaryTextColor, width: 2)
+              : null,
+          color: isCompleted ? primaryColor : null,
+        ),
+        child: isCompleted ? const Icon(CupertinoIcons.checkmark_alt) : null,
       ),
-      child: isCompleted ? const Icon(CupertinoIcons.checkmark_alt) : null,
     );
   }
 }
-class PriortyCheckBox extends StatelessWidget {
-  final Color? color;
-  final bool isCompleted;
 
-  PriortyCheckBox(this.isCompleted, [this.color]);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 24,
-      height: 24,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: color,
-      ),
-      child: isCompleted ? const Icon(CupertinoIcons.checkmark_alt) : null,
-    );
-  }
-}
 
 class EditPersonScreen extends StatelessWidget {
   @override
@@ -312,7 +344,8 @@ class EditPersonScreen extends StatelessWidget {
 
 class EditTaskScreen extends StatefulWidget {
   final Tasks tasks;
-  const EditTaskScreen({Key? key ,required this.tasks}):super(key: key);
+
+  const EditTaskScreen({Key? key, required this.tasks}) :super(key: key);
 
   @override
   State<EditTaskScreen> createState() => _EditTaskScreenState();
@@ -321,24 +354,23 @@ class EditTaskScreen extends StatefulWidget {
 class _EditTaskScreenState extends State<EditTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    TextEditingController _controller = TextEditingController();
+    late final TextEditingController _controller = TextEditingController(text: widget.tasks.name);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add Task"),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
-            final task = Tasks();
-            task.name = _controller.text;
-            task.priority = Priority.low;
-            if (task.isInBox) {
-              task.save();
+            widget.tasks.name = _controller.text;
+            widget.tasks.priority = widget.tasks.priority;
+            if (widget.tasks.isInBox) {
+              widget.tasks.save();
             } else {
               final Box<Tasks> box = Hive.box<Tasks>(openBox);
-              box.add(task);
+              box.add(widget.tasks);
             }
             Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => MyHomePage()));
+                MaterialPageRoute(builder: (context) => MyHomePage()));
           },
           label: const Text("Save Changes")),
       body: Padding(
@@ -348,27 +380,27 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
             Flex(
               direction: Axis.horizontal,
               children: [
-                Flexible(flex: 1,child: PriorityButton(onTap: (){
+                Flexible(flex: 1, child: PriorityButton(onTap: () {
                   setState(() {
                     widget.tasks.priority = Priority.low;
                   });
-                },title: "Low", color: Colors.cyanAccent)),
+                }, title: "Low", color: Colors.cyanAccent , isSelected: widget.tasks.priority == Priority.low,)),
                 const SizedBox(
                   width: 8,
                 ),
-                Flexible(flex: 1,child: PriorityButton(onTap: (){
+                Flexible(flex: 1, child: PriorityButton(onTap: () {
                   setState(() {
                     widget.tasks.priority = Priority.medium;
                   });
-                },title: "Medium", color: Colors.amberAccent)),
+                }, title: "Medium", color: Colors.amberAccent, isSelected: widget.tasks.priority == Priority.medium)),
                 const SizedBox(
                   width: 8,
                 ),
-                Flexible(flex: 1,child: PriorityButton(onTap:(){
+                Flexible(flex: 1, child: PriorityButton(onTap: () {
                   setState(() {
                     widget.tasks.priority = Priority.high;
                   });
-                },title: "High",color:  Colors.redAccent)),
+                }, title: "High", color: Colors.redAccent, isSelected: widget.tasks.priority == Priority.high)),
               ],
             ),
             TextField(
@@ -383,46 +415,45 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
   }
 }
 
-class PriorityButton extends StatefulWidget {
+class PriorityButton extends StatelessWidget {
   final GestureTapCallback onTap;
-  bool isTrue = false;
   Color color;
-  String title ;
+  String title;
+  bool isSelected;
 
-   PriorityButton({Key? key, required this.onTap,required this.title,required this.color }):super(key: key);
-
-  @override
-  State<PriorityButton> createState() => _PriorityButtonState();
-}
-
-class _PriorityButtonState extends State<PriorityButton> {
+  PriorityButton(
+      {Key? key, required this.onTap, required this.title, required this.color , required this.isSelected })
+      :super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-        border: Border.all(color: secondaryTextColor.withOpacity(0.2) ,width: 1),
+        border: Border.all(
+            color: secondaryTextColor.withOpacity(0.2), width: 1),
       ),
       height: 40,
       child: InkWell(
-        onTap: (){
-          setState(() {
-            widget.isTrue = !widget.isTrue;
-
-          });
-        },
+        onTap: onTap,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(widget.title,
-                style: const TextStyle(
-                  color: primaryTextColor,
-                )),
-            PriortyCheckBox(widget.isTrue, widget.color),
-          ],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Text(title,
+            style: const TextStyle(
+              color: primaryTextColor,
+            )),
+        Container(
+          width: 24,
+          height: 24,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: color,
+          ),
+          child: isSelected ? const Icon(CupertinoIcons.checkmark_alt) : null,
         ),
+        ],
       ),
-    );
+    ),);
   }
 }
